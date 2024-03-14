f055761653306ee55e978c71e567e62f40e960cb
ranggahakim05 <ranggahakim05@gmail.com>
2024-03-14 22:18:57 +0700

    login and infosiswa page is done

--- tugasKhusus/urls.py
+++ tugasKhusus/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('main', views.Main, name = "Main Page")
+    path('main', views.Main, name = "Main Page"),
+    path("login", views.LoginPage, name="Login Page"),
+    path("InfoSiswa", views.AuthProcess, name="Info-Siswa-Page"),
 ]
--- tugasKhusus/views.py
+++ tugasKhusus/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+
 from .models import Siswa, NilaiSiswa, Presensi, BabPengajaran, DaftarSiswaKelas, Kelas, Guru, KomponenPenilaian, MataPelajaran, Karyawan, Kurikulum, User, Feedback
 
 def index(request):
@@ -37,3 +39,44 @@ def Main(request):
                'allFeedback':allFeedback}
     
     return HttpResponse(template.render(context, request))
+
+def LoginPage(request):
+    allSiswa = Siswa.objects.all().values()
+
+    template = loader.get_template('login.html')
+
+    context = {'allSiswa': allSiswa}
+
+
+    return HttpResponse(template.render(context, request))
+
+def AuthProcess(request):
+    
+    if request.method == "POST":
+
+        Input_nik = request.POST.get('nik')
+
+        #return HttpResponse(Input_nik)
+        try:
+            loginSiswa = Siswa.objects.filter(nik = Input_nik).get()
+        except:
+            return HttpResponse("GAGAL!")
+        else:
+            #return HttpResponse(loginSiswa)
+            return HttpResponse(Info_SiswaPage(loginSiswa, request))
+    else:
+        return HttpResponseRedirect('login')
+
+
+
+def Info_SiswaPage(siswa, request):
+
+    namaKelas = DaftarSiswaKelas.objects.filter(siswa= siswa.nik).get().Kelas.nama
+
+    context = {'Siswa': siswa, 'namaKelas':namaKelas}
+    template = loader.get_template('Info_Siswa.html')
+
+    return HttpResponse(template.render(context, request))
+    
+    
+    #return HttpResponse(idKelasSiswa.Kelas.nama)
