5b5c94c99d64465905895f0930bc6b17c4e7763d
ranggahakim05 <ranggahakim05@gmail.com>
2024-03-26 01:46:33 +0700

    3rd update
    Many changes, like new page for guru and karyawan

--- tugasKhusus/urls.py
+++ tugasKhusus/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('main', views.Main, name = "Main Page"),
-    path("login", views.LoginPage, name="Login Page"),
-    path("InfoSiswa", views.AuthProcess, name="Info-Siswa-Page"),
+    path('main', views.Main, name = "Main-Page"),
+    path("login", views.LoginPage, name="Login-Page"),
+    path("InfoUser", views.AuthProcess, name="Info-User"),
+    path("NilaiSiswa", views.NilaiSiswaFunction, name="Nilai-Siswa"),
+    path("Presensi", views.MasukkanPresensi, name="Presensi"),
+    path("LogOut", views.LogOut, name = "Log-Out")
 ]
--- tugasKhusus/views.py
+++ tugasKhusus/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
 from django.template import loader
 from django.urls import reverse
 
 from .models import Siswa, NilaiSiswa, Presensi, BabPengajaran, DaftarSiswaKelas, Kelas, Guru, KomponenPenilaian, MataPelajaran, Karyawan, Kurikulum, User, Feedback
+
+global_isLogin = False
+global_loginUser = None
+global_userType = None
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Tugas Khusus index.")
@@ -54,29 +59,117 @@ def AuthProcess(request):
     
     if request.method == "POST":
 
-        Input_nik = request.POST.get('nik')
+        Input_username = request.POST.get('username')
+        Input_password = request.POST.get('password')
 
         #return HttpResponse(Input_nik)
         try:
-            loginSiswa = Siswa.objects.filter(nik = Input_nik).get()
+            loginUser = User.objects.filter(username = Input_username).get()
         except:
-            return HttpResponse("GAGAL!")
+            return HttpResponse("User tidak ditemukan!")
         else:
             #return HttpResponse(loginSiswa)
-            return HttpResponse(Info_SiswaPage(loginSiswa, request))
+            if loginUser.password == Input_password:
+                
+                global global_isLogin
+                global_isLogin = True
+
+                global global_loginUser
+                global_loginUser = loginUser
+                
+                
+                return HttpResponse(InfoUser(loginUser, request))
+            else:
+                return HttpResponseRedirect('login')
     else:
-        return HttpResponseRedirect('login')
+        if global_isLogin == True:
+            return HttpResponse(InfoUser(global_loginUser, request))
+        else:
+            return HttpResponseRedirect('login')
 
 
 
-def Info_SiswaPage(siswa, request):
+def InfoUser(user, request):
+    
+    listRole = {"Siswa": Siswa, "Guru": Guru, "Karyawan": Karyawan}
 
-    namaKelas = DaftarSiswaKelas.objects.filter(siswa= siswa.nik).get().Kelas.nama
+    userType = str()
+    kelasSiswa = None
+    daftarSiswaKelas = None
+    kelasGuru = None
 
-    context = {'Siswa': siswa, 'namaKelas':namaKelas}
-    template = loader.get_template('Info_Siswa.html')
+    for key, value in listRole.items():
+        try:
+            userData = value.objects.filter(nik = user.nik).get()
+            userType = str(key)
+        except:
+            continue
+        else:
+            break
+
+    if userData == None:
+        userType = 'not found'
+
+    global global_userType
+    global_userType = userType
+
+    if userType == "Siswa":
+        kelasSiswa = DaftarSiswaKelas.objects.filter(siswa = user.nik).get().Kelas.nama
+
+    if userType == "Guru":
+        try:
+            kelasGuru = Kelas.objects.filter(wali_kelas = user.nik).get()
+            daftarSiswaKelas = DaftarSiswaKelas.objects.filter(Kelas = kelasGuru.id).all()
+        except:
+            print("Guru bukan seorang walas")
+
+
+
+    context = {'userData':userData, 'userType':userType, "kelasSiswa": kelasSiswa, "kelasGuru":kelasGuru, "daftarSiswaKelas": daftarSiswaKelas}
+    template = loader.get_template('Info_User.html')
 
     return HttpResponse(template.render(context, request))
+
+
+def NilaiSiswaFunction(request):
+   
+   if global_userType == "Siswa" and global_isLogin == True:
+        
+    nilai = NilaiSiswa.objects.filter(siswa = global_loginUser.nik ).all()
+
+    context = {"siswa": Siswa.objects.filter(nik = global_loginUser.nik).get(), "nilai": nilai, "userType": global_userType}
     
-    
-    #return HttpResponse(idKelasSiswa.Kelas.nama)
+    template = loader.get_template('nilai_Siswa_Page.html')
+    return HttpResponse(template.render(context, request))
+        
+    # return HttpResponse(mapel)
+   else: 
+       return HttpResponseRedirect('InfoUser')
+
+def MasukkanPresensi(request):
+
+    if global_userType == "Guru" and global_isLogin == True:
+
+        mapel = MataPelajaran.objects.filter()
+
+
+        context = {"userType": global_userType}
+
+        template = loader.get_template('Presensi_Page.html')
+        return HttpResponse(template.render(context, request))
+
+    else: 
+       return HttpResponseRedirect('InfoUser')
+
+
+def LogOut(request):
+    global global_isLogin, global_loginUser, global_userType
+    global_isLogin = False
+    global_loginUser = None
+    global_userType = None
+
+    # return HttpResponse("User tidak ditemukan!")
+
+    # return reverse("Login-Page")
+
+    return HttpResponseRedirect('login')
