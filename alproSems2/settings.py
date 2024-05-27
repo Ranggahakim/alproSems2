cf57550038f4957d9d7c37c27a9b4ddfc4b91ebd
ranggahakim05 <ranggahakim05@gmail.com>
2024-05-28 00:59:31 +0700

    WELCOME, POSTGRE!
    - Finally we successful change the database from SQLite to Postgre

--- alproSems2/settings.py
+++ alproSems2/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'alproSems2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'AlproSems2',
+        'USER': 'postgres',
+        'PASSWORD': 'user',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
